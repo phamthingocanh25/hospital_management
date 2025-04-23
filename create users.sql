57deced191b3f2658f14ab351441c145c2c17895
manhmanh39 <148619729+manhmanh39@users.noreply.github.com>
2025-04-23 17:28:45 +0700

    Add files via upload

---
+++ create users.sql
@@ -0,0 +1,8 @@
+drop table if exists users;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'doctor', 'receptionist', 'accountant') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
